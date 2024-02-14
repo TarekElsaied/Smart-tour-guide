f38161b671187cedb9f95a2df8e18c9c08a699bc
Tarek Elsaied <tarekelsaied683@gmail.com>
2024-02-14 18:56:17 +0200

    placeByNameUPdate commit

--- src/modules/place/place.routers.js
+++ src/modules/place/place.routers.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { addPlace, getAllPlaces, getPlaceById } from "./place.controller.js";
+import {
+  addPlace,
+  getAllPlaces,
+  getPlace,
+  getPlaceByName,
+} from "./place.controller.js";
 import { filesUpload } from "../../utils/fielUpload.js";
 
 const placeRouter = express.Router();
 
 placeRouter.get("/", getAllPlaces);
-placeRouter.get("/:id", getPlaceById);
+placeRouter.get("/:id", getPlace);
+placeRouter.get("/search/:name", getPlaceByName);
 placeRouter.post("/", filesUpload("path"), addPlace);
 
 export default placeRouter;
