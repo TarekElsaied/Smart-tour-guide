bb85493dfde694322a6b7858fd8c73fd0267e40b
Tarek Elsaied <tarekelsaied683@gmail.com>
2024-02-20 03:55:52 +0200

    logi55  commit

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -10,7 +10,7 @@ import {
 const userRouter = express.Router();
 
 userRouter.post("/signUp", signUp);
-userRouter.get("/signIn", signIn);
+userRouter.post("/signIn", signIn);
 userRouter.post("/resetPasswordLink", resetLink);
 userRouter.post("/code-confirmation", codeConfirmation);
 userRouter.post("/resetPassword", forgotPassword);
